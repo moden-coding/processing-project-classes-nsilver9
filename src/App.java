1feb5d2304162b4c19986d04c22286e4527d9a50
nsilver9 <28nsilver@gannacademy.org>
2024-12-06 11:34:19 -0500

    unfinished

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 
 public class App extends PApplet{
 
+    Pacman pacman;
+
     Coin coin;
     ArrayList<Coin> coins;
     public static void main(String[] args)  {
@@ -12,7 +14,7 @@ public class App extends PApplet{
 
 
     public void setup(){
-        background(0);
+        background(0, 0, 40);
         coins = new ArrayList<>();
 
         for(int i = 0; i < 75; i++) {
@@ -20,6 +22,8 @@ public class App extends PApplet{
             float y = random(50, height - 50);
             float size = 25;
             coins.add(new Coin(x, y, size));
+
+        pacman = new Pacman(300, 300, 50);
         }
     }
 
@@ -28,14 +32,45 @@ public class App extends PApplet{
     }
 
     public void draw(){
+
+        background(0, 0, 40);
         for (Coin c: coins) {
             drawCoin(c);
+
+        fill (255, 255, 0);
+        ellipse(pacman.getX(), pacman.getY(), pacman.getSize(), pacman.getSize());
         }
     }
 
+    public void keyPressed() {
+        float step = 5;
+
+        if(keyCode == LEFT) {
+            pacman.moveLeft(step);
+        } else if (keyCode == RIGHT) {
+            pacman.moveRight(step);
+        } else if (keyCode == UP) {
+            pacman.moveUp(step);
+        } else if (keyCode == DOWN) {
+            pacman.moveDown(step);
+        }
+
+        //I used ChatGPT here to make contstrains so that pacman can't leave the screen
+        pacman = new Pacman(
+            constrain(pacman.getX(), pacman.getSize() / 2, width - pacman.getSize() / 2),
+            constrain(pacman.getY(), pacman.getSize() / 2, height - pacman.getSize() / 2),
+            pacman.getSize()
+        );
+    }
+
     public void drawCoin(Coin coin){
-        fill(255, 255, 0);
+        fill(255, 205, 0);
         stroke(0);
         ellipse(coin.getX(), coin.getY(), coin.getSize(), coin.getSize());
     }
-}
+
+    //I used ChatGPT to check help create methods and variables to check if pacman and the coin are touching. If yes, the coin disapears 
+  
+
+    }
+
